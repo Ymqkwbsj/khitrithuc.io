b6b510015376e2252f7b245391c220887fd7a039
Nhat ANh <123884792+Ymqkwbsj@users.noreply.github.com>
2025-03-04 14:40:26 +0700

    Update script.js

--- next/login/script.js
+++ next/login/script.js
@@ -1,14 +1,22 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    const users = ['user1', 'user2', 'user3', 'user4', 'user5', 'user6', 'user7', 'user8', 'user9', 'user10', 'admin'];
-    if ((users.includes(username) && password === '123')) {
-        window.location.href = 'https://translate.google.com/?hl=vi&sl=auto&tl=vi&op=translate';
-    } else if (username === 'root' && password === 'root') {
-        window.location.href = 'https://www.google.com';
-    } else {
-        alert('Invalid username or password');
+function handleLogin(event) {
+    event.preventDefault(); // Ngăn chặn form submit mặc định
+    
+    const users = {};
+    for (let i = 1; i <= 9; i++) {
+        users[`user${i}`] = "123";
     }
-});
+    users["root"] = "root";
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    
+    if (users[username] && users[username] === password) {
+        if (username === "root") {
+            window.location.href = "https://github.com/Ymqkwbsj/khitrithuc.io/tree/11dfc8b63fb04ae3df76d403947f14cc0f065c6b/next";
+        } else {
+            window.location.href = "https://www.google.com";
+        }
+    } else {
+        alert("Bạn đã đăng nhập sai!");
+    }
+}
